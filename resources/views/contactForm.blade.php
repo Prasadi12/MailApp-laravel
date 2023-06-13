b88d8d010b6d0858eed954dfb0d9774f31dd7e07
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-06-13 14:59:28 +0530

    set route

---
+++ resources/views/contactForm.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" cantent="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="{{ asset('bootstarp.min.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3 mt-5">
+                <h4>Contact Us</h4><hr>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
