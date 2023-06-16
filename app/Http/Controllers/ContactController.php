ccfabc1ece7caf5722f16ee41243f4adeeaf65e0
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-06-16 11:14:53 +0530

    blade create

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -23,6 +24,12 @@ class ContactController extends Controller
                 'subject'=>$request->subject,
                 'body'=>$request->message,
             ];
+
+            Mail::send('email-template', $mail_data, function($message) use ($mail_data){
+                $message->to($mail_data['recipient'])
+                        ->from($mail_data['fromEmail'],$mail_data['fromName'])
+                        ->subject($mail_data['subject']);
+            });
         }else{
             return redirect()->back()->withInput()->with('error', 'Check your internet connection');
         }
